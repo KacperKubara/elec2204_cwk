1bc286f6b5f666d05ad73a0cac0723a87f83facc
KacperKubara <kjk1u17@soton.ac.uk>
2019-03-11 17:59:00 +0000

    splitting words still not working

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <string.h>
 #include <iostream>
 #include <fstream>
 #include "configure.h"
@@ -23,7 +23,7 @@ int main()
 }
 
 void read_data(void)
-{   
+{
     string buffer;
     int temp;
     int i = 0;
@@ -35,14 +35,39 @@ void read_data(void)
             break;
         temp = parse_assembly(buffer);
         cout << buffer << endl;
+        cout << std::hex << temp << endl;
         update_program_data(temp, i);
         i++;
     }
     file.close();
 }
 int parse_assembly(string data)
-{
-    ;
+{   char *c_data = data.c_str();
+    char *opcode;
+    int op_code;
+    int rs1;
+    int rs0;
+    int rd;
+
+    opcode = strtok(c_data, " ");
+
+    if(opcode == "LD")  op_code = 0x00;
+    if(opcode == "STR") op_code = 0x01;
+    if(opcode == "ADD") op_code = 0x02;
+    if(opcode == "SUB") op_code = 0x03;
+    if(opcode == "MLT") op_code = 0x04;
+    if(opcode == "DIV") op_code = 0x05;
+    if(opcode == "END") op_code = 0x0F;
+
+    rs1 = stoi(strtok(NULL, " "));
+    rs0 = stoi(strtok(NULL, " "));
+    rd  = stoi(strtok(NULL, " "));
+
+    op_code = op_code << 6*4;
+    rs1 = rs1 << 4*4;
+    rs0 = rs0 << 2*4;
+
+    return (op_code | rs1 | rs0 | rd );
 }
 void update_program_data(int data, int n)
 {
