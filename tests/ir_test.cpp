09edd8e4d626c1705edfbe63414b607acf1955d2
KacperKubara <kjk1u17@soton.ac.uk>
2019-03-11 20:37:46 +0000

    updated unit tests

--- tests/ir_test.cpp
+++ tests/ir_test.cpp
@@ -2,42 +2,45 @@
 
 #include "catch.hpp"   // library for unit tests
 #include "../cpu/ir.h" //class to be tested
-
 using namespace std;
 
 TEST_CASE("IR Test")
 {
     Ir instructionReg(0, RAM_PROGRAM_ADDRESS);
+    Ram ram0(true);
+    instructionReg.get_ram(ram0);
+
     SECTION("Initialisation TEST")
     {
-        REQUIRE(instructionReg.op_code == 0);
-        REQUIRE(instructionReg.rs0 == 1);
-        REQUIRE(instructionReg.rs1 == 2);
-        REQUIRE(instructionReg.rd  == 3);
-    }
-    SECTION("set_next_instruction TEST0")
-    {
-        instructionReg.set_next_instruction();
-        REQUIRE(instructionReg.op_code == 1);
+        instructionReg.get_instruction();
+        REQUIRE(instructionReg.op_code == 2);
+        REQUIRE(instructionReg.rs1 == 1);
         REQUIRE(instructionReg.rs0 == 2);
-        REQUIRE(instructionReg.rs1 == 3);
-        REQUIRE(instructionReg.rd  == 4);
+        REQUIRE(instructionReg.rd == 3);
+    }
+    SECTION("get_next_instruction TEST0")
+    {
+        instructionReg.get_next_instruction();
+        REQUIRE(instructionReg.op_code == 3);
+        REQUIRE(instructionReg.rs1 == 2);
+        REQUIRE(instructionReg.rs0 == 1);
+        REQUIRE(instructionReg.rd == 4);
 
-        SECTION("set_next_instruction TEST1")
+        SECTION("get_next_instruction TEST1")
         {
-            instructionReg.set_next_instruction();
-            REQUIRE(instructionReg.op_code == 2);
-            REQUIRE(instructionReg.rs0 == 3);
-            REQUIRE(instructionReg.rs1 == 4);
-            REQUIRE(instructionReg.rd  == 5);
-        }
-        SECTION("set_instruction TEST0")
-        {
-            instructionReg.set_instruction();
-            REQUIRE(instructionReg.op_code == 1);
+            instructionReg.get_next_instruction();
+            REQUIRE(instructionReg.op_code == 4);
+            REQUIRE(instructionReg.rs1 == 1);
             REQUIRE(instructionReg.rs0 == 2);
-            REQUIRE(instructionReg.rs1 == 3);
-            REQUIRE(instructionReg.rd  == 4);
+            REQUIRE(instructionReg.rd == 5);
+        }
+        SECTION("get_instruction TEST0")
+        {
+            instructionReg.get_instruction();
+            REQUIRE(instructionReg.op_code == 3);
+            REQUIRE(instructionReg.rs1 == 2);
+            REQUIRE(instructionReg.rs0 == 1);
+            REQUIRE(instructionReg.rd == 4);
         }
     }
 }
--- tests/pc_test.cpp
+++ tests/pc_test.cpp
@@ -2,16 +2,17 @@
 
 #include "catch.hpp" // library for unit tests
 #include "../cpu/pc.h" //class to be tested
+#include "../configure.h"
 
 using namespace std;
 
 TEST_CASE("PC Test"){
-    Pc programCounter(5, 0);
+    Pc programCounter(5, RAM_PROGRAM_ADDRESS);
     SECTION("Counter Test 0"){
-        REQUIRE(programCounter.get_counter() == 5);
+        REQUIRE(programCounter.get_counter() == 5 + RAM_PROGRAM_ADDRESS);
     }
     SECTION("Counter Test 1"){
         programCounter.increment();
-        REQUIRE(programCounter.get_counter() == 6);
+        REQUIRE(programCounter.get_counter() == 6 + RAM_PROGRAM_ADDRESS);
     }
 }
